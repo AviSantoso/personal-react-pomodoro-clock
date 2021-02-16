25b88de0b5b964a162962a97856ce0e3521972ca
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-16 09:52:29 +0800

    setup basic repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import "./styles.css";
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+import "./global.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
+  }
+}));
 
 export default function App() {
+  const styles = useStyles();
   return (
-    <div className="App">
+    <div className={styles.root}>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
