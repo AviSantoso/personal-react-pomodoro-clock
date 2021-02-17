c13553bfe355d3215acc4f853cc47dcf863c50c8
Aviciena Santoso <avi.santoso@gmail.com>
2021-02-17 09:47:54 +0800

    setup pomodoro context for use app wide

--- src/PomodoroContext.tsx
+++ src/PomodoroContext.tsx
@@ -1,3 +1,31 @@
 import React from "react";
 
-const PomodoroContext = React.
+export interface IPomodoroContext {
+  breakLength: number;
+  sessionLength: number;
+  secondsLeft: number;
+  startTimer: () => void;
+  stopTimer: () => void;
+  resetTimer: () => void;
+  incrementBreak: () => void;
+  decrementBreak: () => void;
+  incrementSession: () => void;
+  decrementSession: () => void;
+}
+
+export const defaultPomodoroContext: IPomodoroContext = {
+  breakLength: 5,
+  sessionLength: 25,
+  secondsLeft: 5 * 25 * 60,
+  startTimer: () => {},
+  stopTimer: () => {},
+  resetTimer: () => {},
+  incrementBreak: () => {},
+  decrementBreak: () => {},
+  incrementSession: () => {},
+  decrementSession: () => {}
+};
+
+export const PomodoroContext = React.createContext<IPomodoroContext>(
+  defaultPomodoroContext
+);
